018d6ef3d1f9326d6b88e5f025ef578b4d6dcd34
Shaun Anderson <shaun.anderson@outlook.com>
2018-09-19 20:05:13 +1000

    Added subclass of UISegmentedControl.

---
+++ Formed/View/UIControls/FormedSegmentedControl.h
@@ -0,0 +1,16 @@
+//
+//  Form+UISegmentedControl.h
+//  Formed
+//
+//  Created by Shaun Anderson on 19/9/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FormedSegmentedControl : UISegmentedControl
+@property int controlID;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Formed/View/UIControls/FormedSegmentedControl.m
@@ -0,0 +1,21 @@
+//
+//  Form+UISegmentedControl.m
+//  Formed
+//
+//  Created by Shaun Anderson on 19/9/18.
+//
+
+#import "FormedSegmentedControl.h"
+
+@implementation FormedSegmentedControl
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
