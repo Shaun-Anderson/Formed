47219f396cfec4c820b5092b2462e7f770e21f01
Shaun Anderson <shaun.anderson@outlook.com>
2018-09-21 15:50:59 +1000

    Added form header and removed form section.

---
+++ Formed/Model/FormHeader.h
@@ -0,0 +1,16 @@
+//
+//  FormHeader.h
+//  Formed
+//
+//  Created by Shaun Anderson on 21/9/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FormHeader : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Formed/Model/FormHeader.m
@@ -0,0 +1,12 @@
+//
+//  FormHeader.m
+//  Formed
+//
+//  Created by Shaun Anderson on 21/9/18.
+//
+
+#import "FormHeader.h"
+
+@implementation FormHeader
+
+@end
